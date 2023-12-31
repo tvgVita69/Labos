00f240cdd623c74825d1282835949c761464f6db
Vitaly <98489171+tvgVita69@users.noreply.github.com>
2023-12-31 17:12:08 +0300

    Add files via upload

---
+++ lab_02.py
@@ -0,0 +1,23 @@
+# # Модуль 2. Практическая работа
+
+# ## Игра с геометрическими фигурами
+
+# import abc
+# 
+# class IShape(abc.ABC):
+#     '''Интерфейс для реализации геометрических фигур'''
+#     
+#     @abc.abstractmethod
+#     def get_perimeter(self):
+#         '''Возвращает периметр фигуры'''
+#         pass
+# 
+#     @abc.abstractmethod
+#     def get_area(self):
+#         '''Возвращает площадь фигуры'''
+#         pass
+# 
+#     @abc.abstractmethod
+#     def get_description(self):
+#         '''Возвращает произвольное описание фигуры'''
+#         pass
---
+++ lab_03.py
@@ -0,0 +1,39 @@
+# # Модуль 5. Практическая работа
+
+# ## Создание простого интерпретатора арифметических выражений
+
+# ### Основное задание
+
+# import abc
+# 
+# class InterpreterAbstract(abc.ABC):
+#     '''Интерпретатор кода'''
+#     
+#     def __init__(self, code):
+#         '''Принимает код'''
+#         self.code = code
+#     
+#     def execute(self):
+#         '''Запускает механизм исполнения кода
+#         Возвращает результат исполнения кода'''
+#         return self._parse()
+#         
+#     @abc.abstractmethod
+#     def _parse(self):
+#         '''Осуществляет парсинг кода.
+#         Вызывает _evaluate для исполнения выражений
+#         Возвращает результат исполнения кода в excecute''' 
+#         pass
+#     
+#     @abc.abstractmethod
+#     def _evaluate(self, code):
+#         '''Осуществляет вычисление выражения
+#         Возвращает результат выражения в _parse'''      
+#         pass
+
+
+# interpreter = Interpreter('(1+((2+3)*(4*5)))')
+# print(interpreter.execute()) # 101
+# 
+# interpreter = Interpreter('(2+((2*3)/(4^5)))')
+# print(interpreter.execute()) # 2
